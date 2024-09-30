f77aa4a1e647b885d2915397c91e392f8e4b554e
Minwa <pcr.wpd.mawin@gmail.com>
2024-09-30 11:59:28 +0700

    Change player Hp.

--- entity_factory.py
+++ entity_factory.py
@@ -4,7 +4,7 @@ from components.ai_component import HostileEnemy
 from components.fighter_component import Fighter
 from entity import Actor
 
-player = Actor(char="@", color=(255, 255, 255), name="Player", ai_class=HostileEnemy, fighter=Fighter(hp=1, dv=2, attack=6),)
+player = Actor(char="@", color=(255, 255, 255), name="Player", ai_class=HostileEnemy, fighter=Fighter(hp=100, dv=200, attack=60),)
 
 orc = Actor(char="o", color=(63, 127, 63), name="Orc", ai_class=HostileEnemy, fighter=Fighter(hp=10, dv=0, attack=20),)
 troll = Actor(char="T", color=(0, 127, 0), name="Troll", ai_class=HostileEnemy, fighter=Fighter(hp=16, dv=1, attack=20),)
